dc7ee3715abfddd202f59b302f9cc22091efec70
Andrew Hany <andrewhany260@gmail.com>
2026-07-09 18:13:50 +0300

    feat(downloads): DownloadManager contract + test fake

---
+++ src/downloads/DownloadManager.js
@@ -0,0 +1,20 @@
+/**
+ * @typedef {Object} DownloadEvent
+ * @property {string} id
+ * @property {'progress'|'done'|'error'} type
+ * @property {number} [bytesDone]
+ * @property {number} [bytesTotal]
+ * @property {string} [error]
+ */
+
+/**
+ * @typedef {Object} DownloadManager
+ * @property {(task:{id:string,url:string,localPath:string})=>void} start
+ * @property {(id:string)=>void} pause
+ * @property {(id:string)=>void} resume
+ * @property {(id:string)=>void} cancel
+ * @property {(handler:(e:DownloadEvent)=>void)=>()=>void} subscribe
+ * @property {()=>Promise<void>} reattach
+ * @property {()=>Promise<number>} freeBytes
+ */
+export {};
